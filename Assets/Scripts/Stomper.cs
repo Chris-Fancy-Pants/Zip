3cc8787e96440137e80017a7488469fa0cb398d9
Chris Moltzen <chrristo.moltzen@gmail.com>
2018-01-29 11:44:06 +1100

    Stompers working

---
+++ Assets/Scripts/Stomper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stomper : MonoBehaviour {
+
+
+    public Transform openPosition;
+    public Transform closedPosition;
+
+    public AudioSource doorStopSound;
+
+    public float speed = 10f;
+
+    public enum MoveDirection
+    {
+        up,
+        down
+    };
+
+    public MoveDirection direction = MoveDirection.down;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(direction == MoveDirection.down)
+        {
+            if(transform.position.y > closedPosition.position.y)
+            {
+                //move Down
+                transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+            } else
+            {
+                direction = MoveDirection.up;
+                doorStopSound.Play();
+            }
+        } else if(direction == MoveDirection.up)
+        {
+            if(transform.position.y < openPosition.position.y)
+            {
+                // move up
+                transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+            } else
+            {
+                direction = MoveDirection.down;
+            }
+        }
+		
+	}
+}
