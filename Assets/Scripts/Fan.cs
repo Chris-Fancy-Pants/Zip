2f74734a13f71870093872deb002e9217e0b5813
Chris-Fancy-Pants <christo@Christophers-MacBook-Pro.local>
2018-04-14 07:42:30 +1000

    Spinning Fans and other background art added

---
+++ Assets/Scripts/Fan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+	public Transform fanBlades;
+	public float speed = 1f;
+	float rot = 0;
+
+	// Use this for initialization
+	void Start () {
+		rot = Random.Range (0, 360);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rot += speed * Time.deltaTime;
+
+		if (rot > 360) {
+			rot = rot - 360;
+		}
+
+		fanBlades.eulerAngles = new Vector3 (0, 0, rot);
+	
+
+	}
+}
